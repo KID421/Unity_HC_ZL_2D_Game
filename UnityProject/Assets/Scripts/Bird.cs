8aa728b1a9dac5b4bd47fb1bcd084c4d619c8afe
KID <50666207+KID421@users.noreply.github.com>
2019-12-05 19:43:22 +0800

    水管預製物添加加分區域並設定觸發

--- UnityProject/Assets/Scripts/Bird.cs
+++ UnityProject/Assets/Scripts/Bird.cs
@@ -13,6 +13,10 @@ public class Bird : MonoBehaviour
 
     public GameManager gm;
 
+    [Header("音效區域")]
+    public AudioSource aud;
+    public AudioClip soundJump, soundHit, soundAdd;
+
     /// <summary>
     /// 小雞跳躍功能
     /// </summary>
@@ -31,6 +35,8 @@ public class Bird : MonoBehaviour
             r2d.gravityScale = 1;                   // 剛體.重力 = 1
             r2d.Sleep();                            // 剛體.睡覺()
             r2d.AddForce(new Vector2(0, jump));     // 剛體.增加推力(二維向量)
+
+            aud.PlayOneShot(soundJump, 1.5f);       // 音源.播放一次(音效片段，音量)
         }
 
         // velocity 剛體加速度 (x, y) 左右 x, 上下 y
@@ -44,6 +50,7 @@ public class Bird : MonoBehaviour
     {
         dead = true;
         gm.GameOver();
+        aud.PlayOneShot(soundHit, 1.5f);
     }
 
     /// <summary>
